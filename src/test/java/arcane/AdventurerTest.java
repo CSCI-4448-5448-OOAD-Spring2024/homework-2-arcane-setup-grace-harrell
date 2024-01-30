e42db1db949de61447fefd6b51f3bbe7bff355ab
grace-harrell <89498239+grace-harrell@users.noreply.github.com>
2024-01-29 22:25:12 -0700

    created Adventurer class with methods and tests - removed imports

--- src/test/java/arcane/AdventurerTest.java
+++ src/test/java/arcane/AdventurerTest.java
@@ -3,9 +3,6 @@ package arcane;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.util.Scanner;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class AdventurerTest {
     @Test
