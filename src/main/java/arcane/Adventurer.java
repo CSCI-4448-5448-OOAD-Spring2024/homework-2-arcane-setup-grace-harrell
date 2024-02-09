1b1bc9fdefee4fe7696abe73d84fa5c8395b938a
grace-harrell <89498239+grace-harrell@users.noreply.github.com>
2024-02-09 13:56:41 -0700

    added logback dependancy to gradle

--- src/main/java/arcane/Adventurer.java
+++ src/main/java/arcane/Adventurer.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Adventurer extends Character{
     public Adventurer(){
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Hello Adventurer! Please enter your name: ");
         String newAdventurer = scan.nextLine();
