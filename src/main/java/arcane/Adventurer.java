10c85d22db6ea6841c7b9fa699810bb6b9272031
grace-harrell <89498239+grace-harrell@users.noreply.github.com>
2024-02-12 13:33:24 -0700

    added logback dependancy to gradle

--- src/main/java/arcane/Adventurer.java
+++ src/main/java/arcane/Adventurer.java
@@ -1,11 +1,17 @@
 package arcane;
 
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Adventurer extends Entity{
+
+    private static final Logger logger = Logger.getLogger(String.valueOf(Adventurer.class));
+
     public Adventurer(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello Adventurer! Please enter your name: ");
+        logger.info("Hello Adventurer! Please enter your name: ");
         String newAdventurer = scan.nextLine();
         setName(newAdventurer);
     }
--- src/main/java/arcane/Arcane.java
+++ src/main/java/arcane/Arcane.java
@@ -1,6 +1,11 @@
 package arcane;
 
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
 public class Arcane {
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(Arcane.class);
     public void gameOver(Boolean over, Adventurer adventurer, Creature creature) {
         if (over){
             System.out.println("Yay, Adventurer " + adventurer.getName() + " wins.");
@@ -17,7 +22,7 @@ public class Arcane {
         Creature creature = new Creature();
         Cave cave = new Cave(creature,adventurer);
 
-        System.out.println("Starting play...");
+        logger.info("Starting play...");
         while (adventurer.isAlive() && creature.isAlive()){
             turn_id += 1;
             Turn turn = new Turn(turn_id, cave, creature, adventurer);
