6b795f52392b608ff160124dc650c144290b9bed
grace-harrell <89498239+grace-harrell@users.noreply.github.com>
2024-02-28 07:40:51 -0700

    Added notes for implementation in main

--- src/main/java/arcane/GameConfiguration.java
+++ src/main/java/arcane/GameConfiguration.java
@@ -6,6 +6,14 @@ import org.apache.commons.cli.DefaultParser;
 public class GameConfiguration {
     public static void main(String[] args){
         CommandLineParser parser = new DefaultParser();
+        // takes arguments in the following format....
+//        "--numberOfRooms", "4",
+//                "--numberOfAdventurers", "2",
+//                "--numberOfCreatures", "2",
+//                "--numberOfFoodItems", "2"
 
+        // uses arguments to create new Factories...
+        // i.e. AdventurerFactory af = new AdventurerFactory(numCreatures)
+        // creates Cave ith Builder
     }
 }
